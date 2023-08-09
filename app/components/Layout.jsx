bdb076c7b8647eeec61a5f00a809c563f4d2791a
Hardik-Softpulse <softpulsedev010@gmail.com>
2023-08-09 18:27:25 +0530

    Header  changes in layout.jsx

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,3 +1,10 @@
+import paypal from '../img/paypal.png';
+import mastercard from '../img/mastercard.png';
+import americanExpress from '../img/american-express.png';
+import masestro from '../img/masestro.png';
+import visa from '../img/visa.png';
+import dinersClub from '../img/diners-club.png';
+import logo from '../img/logo.png';
 import {useParams, Form, Await, useMatches} from '@remix-run/react';
 import {useWindowScroll} from 'react-use';
 import {Disclosure} from '@headlessui/react';
@@ -70,21 +77,12 @@ function Header({title, menu}) {
 
   return (
     <>
-      <CartDrawer isOpen={isCartOpen} onClose={closeCart} />
-      {menu && (
-        <MenuDrawer isOpen={isMenuOpen} onClose={closeMenu} menu={menu} />
-      )}
+      {/* <CartDrawer isOpen={isCartOpen} onClose={closeCart} /> */}
       <DesktopHeader
         isHome={isHome}
         title={title}
         menu={menu}
         openCart={openCart}
-      />
-      <MobileHeader
-        isHome={isHome}
-        title={title}
-        openCart={openCart}
-        openMenu={openMenu}
       />
     </>
   );
@@ -106,167 +104,106 @@ function CartDrawer({isOpen, onClose}) {
   );
 }
 
-export function MenuDrawer({isOpen, onClose, menu}) {
-  return (
-    <Drawer open={isOpen} onClose={onClose} openFrom="left" heading="Menu">
-      <div className="grid">
-        <MenuMobileNav menu={menu} onClose={onClose} />
-      </div>
-    </Drawer>
-  );
-}
-
-function MenuMobileNav({menu, onClose}) {
-  return (
-    <nav className="grid gap-4 p-6 sm:gap-6 sm:px-12 sm:py-8">
-      {/* Top level menu items */}
-      {(menu?.items || []).map((item) => (
-        <span key={item.id} className="block">
-          <Link
-            to={item.to}
-            target={item.target}
-            onClick={onClose}
-            className={({isActive}) =>
-              isActive ? 'pb-1 border-b -mb-px' : 'pb-1'
-            }
-          >
-            <Text as="span" size="copy">
-              {item.title}
-            </Text>
-          </Link>
-        </span>
-      ))}
-    </nav>
-  );
-}
-
-function MobileHeader({title, isHome, openCart, openMenu}) {
-  // useHeaderStyleFix(containerStyle, setContainerStyle, isHome);
-
+function DesktopHeader({isHome, menu, title}) {
   const params = useParams();
 
   return (
-    <header
-      role="banner"
-      className={`${
-        isHome
-          ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-          : 'bg-contrast/80 text-primary'
-      } flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`}
-    >
-      <div className="flex items-center justify-start w-full gap-4">
-        <button
-          onClick={openMenu}
-          className="relative flex items-center justify-center w-8 h-8"
-        >
-          <IconMenu />
-        </button>
-        <Form
-          method="get"
-          action={params.locale ? `/${params.locale}/search` : '/search'}
-          className="items-center gap-2 sm:flex"
-        >
-          <button
-            type="submit"
-            className="relative flex items-center justify-center w-8 h-8"
-          >
-            <IconSearch />
-          </button>
-          <Input
-            className={
-              isHome
-                ? 'focus:border-contrast/20 dark:focus:border-primary/20'
-                : 'focus:border-primary/20'
-            }
-            type="search"
-            variant="minisearch"
-            placeholder="Search"
-            name="q"
-          />
-        </Form>
+    <header className="site-header">
+      <div className="promotion-bar text-center">
+        <div className="container">Free shipping on order above $20.</div>
       </div>
-
-      <Link
-        className="flex items-center self-stretch leading-[3rem] md:leading-[4rem] justify-center flex-grow w-full h-full"
-        to="/"
-      >
-        <Heading
-          className="font-bold text-center leading-none"
-          as={isHome ? 'h1' : 'h2'}
-        >
-          {title}
-        </Heading>
-      </Link>
-
-      <div className="flex items-center justify-end w-full gap-4">
-        <AccountLink className="relative flex items-center justify-center w-8 h-8" />
-        <CartCount isHome={isHome} openCart={openCart} />
-      </div>
-    </header>
-  );
-}
-
-function DesktopHeader({isHome, menu, openCart, title}) {
-  const params = useParams();
-  const {y} = useWindowScroll();
-  return (
-    <header
-      role="banner"
-      className={`${
-        isHome
-          ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-          : 'bg-contrast/80 text-primary'
-      } ${
-        !isHome && y > 50 && ' shadow-lightHeader'
-      } hidden h-nav lg:flex items-center sticky transition duration-300 backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-8 px-12 py-8`}
-    >
-      <div className="flex gap-12">
-        <Link className="font-bold" to="/" prefetch="intent">
-          {title}
-        </Link>
-        <nav className="flex gap-8">
-          {/* Top level menu items */}
-          {(menu?.items || []).map((item) => (
-            <Link
-              key={item.id}
-              to={item.to}
-              target={item.target}
-              prefetch="intent"
-              className={({isActive}) =>
-                isActive ? 'pb-1 border-b -mb-px' : 'pb-1'
-              }
-            >
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-      </div>
-      <div className="flex items-center gap-1">
-        <Form
-          method="get"
-          action={params.locale ? `/${params.locale}/search` : '/search'}
-          className="flex items-center gap-2"
-        >
-          <Input
-            className={
-              isHome
-                ? 'focus:border-contrast/20 dark:focus:border-primary/20'
-                : 'focus:border-primary/20'
-            }
-            type="search"
-            variant="minisearch"
-            placeholder="Search"
-            name="q"
-          />
-          <button
-            type="submit"
-            className="relative flex items-center justify-center w-8 h-8 focus:ring-primary/5"
-          >
-            <IconSearch />
-          </button>
-        </Form>
-        <AccountLink className="relative flex items-center justify-center w-8 h-8 focus:ring-primary/5" />
-        <CartCount isHome={isHome} openCart={openCart} />
+      <div className="site-logo-nav">
+        <div className="container">
+          <div className="row flxnwrp">
+            <div className="st-col st-nav-icon icx dfx flx-50 visible-x">
+              <a href="javascript:void(0)" className="st-nav-ic st-nav-trigger">
+                <span></span>
+                <span></span>
+                <span></span>
+              </a>
+              <a
+                href="javascript:void(0)"
+                className="st-nav-ic st-nav-search visible-x"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 12 12"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.18 8.5C6.41 9.92 3.83 9.81 2.19 8.18C0.43 6.42 0.42 3.57 2.18 1.81C3.93 0.05 6.78 0.05 8.54 1.8C10.18 3.44 10.3 6.03 8.88 7.79L11.74 10.64L11.03 11.35L8.18 8.5ZM2.89 2.52C1.52 3.89 1.52 6.1 2.89 7.47C4.26 8.83 6.47 8.83 7.84 7.47L7.85 7.46C9.21 6.09 9.2 3.88 7.84 2.51C6.47 1.15 4.25 1.15 2.89 2.52Z"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="st-col st-nav-menu flx-50 hidden-x">
+              <ul className="site-nav">
+                {(menu?.items || []).map((item) => (
+                  <li key={item.id} prefetch="intent" className="st-nav-li">
+                    <a
+                      href={item.to}
+                      target={item.target}
+                      className="st-nav-link"
+                    >
+                      {' '}
+                      {item.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="st-col st-nav-logo flx-auto">
+              <a href="#">
+                <img src={logo} />
+              </a>
+            </div>
+            <div className="st-col st-nav-icon dfx flxwrp flxend flx-50">
+              <a
+                href="javascript:void(0)"
+                className="st-nav-ic st-nav-search hidden-x"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 12 12"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.18 8.5C6.41 9.92 3.83 9.81 2.19 8.18C0.43 6.42 0.42 3.57 2.18 1.81C3.93 0.05 6.78 0.05 8.54 1.8C10.18 3.44 10.3 6.03 8.88 7.79L11.74 10.64L11.03 11.35L8.18 8.5ZM2.89 2.52C1.52 3.89 1.52 6.1 2.89 7.47C4.26 8.83 6.47 8.83 7.84 7.47L7.85 7.46C9.21 6.09 9.2 3.88 7.84 2.51C6.47 1.15 4.25 1.15 2.89 2.52Z"
+                  />
+                </svg>
+              </a>
+              <a href="account/details" className="st-nav-ic st-nav-user">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 62 72"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M48 17C48 26.35 40.35 34 31 34C21.65 34 14 26.35 14 17C14 7.65 21.65 0 31 0C40.35 0 48 7.65 48 17ZM20 17C20 23.11 24.89 28 31 28C37.11 28 42 23.11 42 17C42 10.89 37.11 6 31 6C24.89 6 20 10.89 20 17ZM31 43C47.97 43 62 55.89 62 72L56 72C56 59.39 44.96 49 31 49C17.04 49 6 59.39 6 72L0 72C0 55.89 14.03 43 31 43Z"
+                  />
+                </svg>
+              </a>
+              <a href="/cart" className="st-nav-ic st-nav-cart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="22"
+                  viewBox="0 0 88 78"
+                >
+                  <path d="M69.26 57.15C65.04 57.15 61.23 59.7 59.61 63.61C58 67.51 58.89 72 61.88 74.99C64.87 77.98 69.36 78.87 73.26 77.26C77.17 75.64 79.72 71.83 79.72 67.61C79.72 66.24 79.45 64.88 78.92 63.61C78.39 62.34 77.62 61.19 76.65 60.22C75.68 59.25 74.53 58.48 73.26 57.95C71.99 57.42 70.63 57.15 69.26 57.15L69.26 57.15Z"></path>
+                  <path d="M48.27 20.43L80.1 20.43L73.77 46.06L28.49 46.06L17.44 -0.06L0.24 -0.06L0.24 5.94L12.71 5.94L23.75 52.06L78.47 52.06L87.76 14.43L48.27 14.43L48.27 20.43L48.27 20.43Z"></path>
+                  <path d="M30.45 57.15C26.22 57.15 22.41 59.69 20.8 63.6C19.18 67.5 20.07 72 23.06 74.99C26.04 77.98 30.54 78.87 34.44 77.26C38.35 75.64 40.9 71.84 40.9 67.61C40.9 66.24 40.63 64.88 40.1 63.61C39.58 62.34 38.81 61.19 37.84 60.22C36.87 59.25 35.72 58.48 34.45 57.95C33.18 57.43 31.82 57.15 30.45 57.15L30.45 57.15Z"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
