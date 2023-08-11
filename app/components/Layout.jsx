d82e4f3dd3ef14f33d8d517bf1f180ce0f43f595
Hardik-Softpulse <softpulsedev010@gmail.com>
2023-08-11 17:39:54 +0530

    Collection page & changes format in layout.jsx

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -121,10 +121,7 @@ function DesktopHeader({isHome, menu, title}) {
                 <span></span>
                 <span></span>
               </a>
-              <a
-                href="#"
-                className="st-nav-ic st-nav-search visible-x"
-              >
+              <a href="#" className="st-nav-ic st-nav-search visible-x">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="22"
@@ -160,10 +157,7 @@ function DesktopHeader({isHome, menu, title}) {
               </a>
             </div>
             <div className="st-col st-nav-icon dfx flxwrp flxend flx-50">
-              <a
-                href="#"
-                className="st-nav-ic st-nav-search hidden-x"
-              >
+              <a href="#" className="st-nav-ic st-nav-search hidden-x">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="22"
@@ -290,7 +284,6 @@ function Footer({menu}) {
   return (
     <Section
       divider={isHome ? 'none' : 'top'}
-      as="footer"
       role="contentinfo"
       className={`footer-section`}
     >
@@ -379,7 +372,7 @@ function FooterMenu({menu}) {
             <div className="stft-col col site-ext-info text-right">
               <h6 className="text-up">Follow us</h6>
               <div className="social-icons dfx flxwrp flxend">
-                <a href="javascript:void(0)">
+                <a href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -389,7 +382,8 @@ function FooterMenu({menu}) {
                     <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
                   </svg>
                 </a>
-                <a href="javascript:void(0)">
+
+                <a href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -409,7 +403,7 @@ function FooterMenu({menu}) {
                     <path d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z" />
                   </svg>
                 </a>
-                <a href="javascript:void(0)">
+                <a href="#">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -444,7 +438,7 @@ function FooterMenu({menu}) {
                 <img src={dinersClub} alt="" />
               </div>
               <p className="copyright-text">
-               &copy; {new Date().getFullYear()} SHOES. ALL RIGHTS RESERVED.
+                &copy; {new Date().getFullYear()} SHOES. ALL RIGHTS RESERVED.
               </p>
             </div>
           </div>
